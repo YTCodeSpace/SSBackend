d737d4ed841154a446f39878768afa3069dd4e9c
Yu Tian <yu.tian1004@gmail.com>
2025-03-03 10:28:46 +0100

    cors problem

--- server.js
+++ server.js
@@ -32,6 +32,27 @@ app.use(
     credentials: true, // Allow cookies to be sent
   })
 );
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+  }
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 const authenticateToken = (req, res, next) => {
   const token = req.cookies.token;
   if (!token) return res.status(403).json({ message: "Unauthorized" });
